9b0dae7d92ac6bba01a472ddeadb506337bd7345
MICHELE CARENA s300467 <s300467@studenti.polito.it>
2025-03-15 19:59:24 +0100

    insert update delete

--- js/db_handler.mjs
+++ js/db_handler.mjs
@@ -29,3 +29,148 @@ export function getItemsWithCondition(tableName, condition, params=[]){
     })
 }
 
+function insertItem(table, columns, values) {
+    return new Promise((resolve, reject) => {
+        
+        const placeholders = columns.map(() => '?').join(', ');
+        const sql = `INSERT INTO ${table} (${columns.join(', ')}) VALUES (${placeholders})`;
+
+        db.run(sql, values, function(err) {
+            if (err) {
+                reject(`Errore nell'inserire il record nella tabella ${table}: ${err.message}`);
+            } else {
+                resolve(`Record aggiunto con successo nella tabella ${table}!`);
+            }
+        });
+    });
+}
+
+function deleteItem(tableName, primaryKeyValue) {
+    return new Promise((resolve, reject) => {
+        let primaryKeyColumn;
+        
+        switch (tableName) {
+            case 'User':
+                primaryKeyColumn = ['username'];
+                break;
+            case 'Allergy':
+                primaryKeyColumn = ['username','allergy'];
+                break;
+            case 'Reservation':
+                primaryKeyColumn = ['username','bagID'];  
+                break;
+            case 'Establishment':
+                primaryKeyColumn = 'establishmentID';  
+                break;
+            case 'Cart':
+                primaryKeyColumn = ['username','bagID'];  
+                break;
+            case 'BagContent':
+                primaryKeyColumn = ['bagID','foodName'];  
+                break;
+            case 'Bag':
+                primaryKeyColumn = ['bagID'];
+                break;
+            default:
+                return reject('Tabella non presente');
+        }
+
+        const sql = `DELETE FROM ${tableName} WHERE ${primaryKeyColumn.map(column => `${column} = ?`).join(' AND ')}`;
+
+        db.run(sql, [primaryKeyValue], function(err) {
+            if (err) {
+                reject('Errore nell\'eliminare il record: ' + err.message);
+            } else {
+                if (this.changes > 0) {
+                    resolve(`Record eliminato con successo dalla tabella ${tableName}!`);
+                } else {
+                    reject('Nessun record trovato con il valore della chiave primaria fornito.');
+                }
+            }
+        });
+    });
+}
+
+function updateItem(tableName, updates, primaryKeyValues) {
+    return new Promise((resolve, reject) => {
+        let primaryKeyColumn;
+        
+        switch (tableName) {
+            case 'User':
+                primaryKeyColumn = ['username'];
+                break;
+            case 'Allergy':
+                primaryKeyColumn = ['username','allergy'];
+                break;
+            case 'Reservation':
+                primaryKeyColumn = ['username','bagID'];  
+                break;
+            case 'Establishment':
+                primaryKeyColumn = 'establishmentID';  
+                break;
+            case 'Cart':
+                primaryKeyColumn = ['username','bagID'];  
+                break;
+            case 'BagContent':
+                primaryKeyColumn = ['bagID','foodName'];  
+                break;
+            case 'Bag':
+                primaryKeyColumn = ['bagID'];
+                break;
+            default:
+                return reject('Tabella non presente');
+        }
+
+        // Creazione della query di aggiornamento dinamica
+        const columnsToUpdate = Object.keys(updates);  // Estrai i nomi delle colonne da aggiornare
+        const setClause = columnsToUpdate.map(column => `${column} = ?`).join(', ');  // Crea la parte SET della query
+
+        const sql = `UPDATE ${tableName} SET ${setClause} WHERE ${primaryKeyColumn.map(column => `${column} = ?`).join(' AND ')}`;
+
+        // Combina i valori da aggiornare con i valori della chiave primaria
+        const params = [...Object.values(updates), ...primaryKeyValues];
+
+        db.run(sql, params, function(err) {
+            if (err) {
+                reject('Errore nell\'aggiornare il record: ' + err.message);
+            } else {
+                if (this.changes > 0) {
+                    resolve(`Record aggiornato con successo nella tabella ${tableName}!`);
+                } else {
+                    reject('Nessun record trovato con i valori della chiave primaria forniti.');
+                }
+            }
+        });
+    });
+}
+
+
+
+
+/*insertItem('User', ['username', 'name', 'password'], ['michi', 'michi care', 'password124'])
+    .then(message => {
+        console.log(message);
+    })
+    .catch(error => {
+        console.error(error); 
+    });
+*/
+
+/*
+deleteItem('User', 'michi') 
+    .then(message => {
+        console.log(message); 
+    })
+    .catch(error => {
+        console.error(error);  
+    });
+*/
+/*
+updateItem('User', { name: 'John Updated', password: 'newPassword123' }, ['john_doe'])  // Passa i nuovi valori e la chiave primaria
+    .then(message => {
+        console.log(message);  // Messaggio di successo
+    })
+    .catch(error => {
+        console.error(error);  // Messaggio di errore
+    });
+*/
--- js/test.mjs
+++ js/test.mjs
@@ -53,6 +53,8 @@ function main(){
     console.log(data);
   });
 
+
+
 }
 
 
